faa3f219d7d13bc8da0d2734f1078f4c74cfe411
Teodora23 <47943153+Teodora23@users.noreply.github.com>
2020-03-25 15:49:51 +0200

    Rethinked whole structure so it respects S

---
+++ Teodora23/RealStateAgency/RealEstateAgency.Core/Commission.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstateAgency.Core
+{
+    public class Commission
+    {   
+        public decimal CommissionValue { get; set; }
+        public decimal CalculateCommission(IAsset asset)
+        {
+            return asset.Price * asset.Commission;
+        }
+    }
+}
---
+++ Teodora23/RealStateAgency/RealEstateAgency.Core/Property.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstateAgency.Core
+{
+    interface IProperty : IAsset
+    {
+        string Adress { get; set; }
+    }
+}
