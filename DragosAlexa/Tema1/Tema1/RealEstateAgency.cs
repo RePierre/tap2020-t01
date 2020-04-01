2b981ce40e4099b43a5cd0e0f8dfbc9b4a917150
DragosAlexa <dragos1899@gmail.com>
2020-04-01 18:32:47 +0300

    Separate classes

---
+++ DragosAlexa/Tema1/Tema1/RealEstateAgency.cs
@@ -0,0 +1,19 @@
+﻿namespace Tema1
+{
+    public abstract class RealEstateAgency {
+        public decimal price;
+        public int yearBuilt;
+        public int surface;
+        
+        public RealEstateAgency(decimal price,int surface, int yearBuilt) {
+            this.price = price;
+            this.surface = surface;
+            this.yearBuilt = yearBuilt;
+        }
+        public abstract decimal calculate_commission();
+        public decimal calculate_price() {
+            return price + calculate_commission();
+        }
+    }
+}
+
---
+++ DragosAlexa/Tema1/Tema1/Studio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tema1
+{
+    public class Studio : RealEstateAgency
+    {
+        public Studio(decimal price,int surface, int yearBuilt) : base(price,surface, yearBuilt){}
+        public override decimal calculate_commission()
+        {
+            if (yearBuilt < 1990)
+            {
+                if (surface < 15 || surface>30)
+                    throw new Exception("No studios with this surface! Please introduce a valid value for surface(15-30 square meters).");
+                else if (surface <= 30)
+                    return price * 0.008m;
+            }
+            else
+            {
+                if (surface < 15|| surface>30)
+                    throw new Exception("No studios with this surface! Please introduce a valid value for surface(15-30 square meters).");
+                else if (surface <=30)
+                    return price * 0.012m;
+            }
+            return price;
+        }
+    }
+}
+
