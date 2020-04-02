8c4ccc8903ff05a2fbac905aca0e65c24ebff4d5
DragosAlexa <dragos1899@gmail.com>
2020-04-02 15:25:03 +0300

    Add currency sign and create RealEstatePrice class

---
+++ DragosAlexa/Tema2/Tema1/RealEstatePrice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Tema2
+{
+    partial class Program
+    {
+        class RealEstatePrice {
+            
+            public static void GetTotalPriceFrom(RealEstateAgency rea, decimal price, TotalPriceCalculator totalPriceCalculator)
+            {
+                var totalPrice = totalPriceCalculator.CalculateTotalPrice(rea, price);
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+                Console.WriteLine("The total price for " + rea.GetType().Name + " is: " + totalPrice+"€");
+            }
+            public static void GetCommissionFrom(RealEstateAgency rea, decimal price, CommissionCalculator commissionCalculator)
+            {
+                var commissionValue = commissionCalculator.CalculateCommission(rea, price);
+                Console.WriteLine("The commission for " + rea.GetType().Name + " is " + commissionValue+ "€");
+            }
+
+        }
+
+    }
+}
+
+
